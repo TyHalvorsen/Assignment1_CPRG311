9b470512b00bcff288b137c8c44c5c73c05bffb1
Marcel Gallardo <116410849+MFGPerez@users.noreply.github.com>
2023-06-13 19:17:49 -0600

    added getAbsolutePath method

--- src/utilities/FileReader.java
+++ src/utilities/FileReader.java
@@ -14,122 +14,126 @@ import shapes.SquarePrism;
 import shapes.TriangularPrism;
 
 /**
- * Class description: Class created a file Reader object with a file path.
- * object has method to populate a Shape array by reading lines in the file and
- * creating shapes. array is then returned.
- *
+ * Class description: Class created a file Reader object with a file path. 
+ * object has method to populate a Shape array by reading lines in the file and creating shapes. 
+ * array is then returned. 
+ * 
  *
  * @author Marcel Gallardo (000827700), Lynn Park , Tyler Halvorsen
- *
- *
+ * 
+ * 
  */
 public class FileReader {
 
-    /* ENCAPSULATED FIELDS */
-    private String path = "";
-    private Shape[] shapeArray;
-    private Debug debug;
+	/* ENCAPSULATED FIELDS */
+	private String path = "";
+	private Shape[] shapeArray;
+	private Debug debug;
 
-    /* CONSTRUCTOR */
-    public FileReader(String path, Debug debugLevel) {
+	/* CONSTRUCTOR */
 
-        this.debug = debugLevel;
-        this.path = path;
+	public FileReader(String path, Debug debugLevel) {
 
-    }
+		this.debug = debugLevel;
+		this.path = path;
 
-    public Shape[] populateShapeArray() {
+	}
 
-        try {
+	public Shape[] populateShapeArray() {
 
-            File file = new File(this.path); // file with path
+		try {
 
-            @SuppressWarnings("resource")
-            Scanner scanner = new Scanner(file); // scanner takes file object
+			File file = new File(this.path); // file with path
 
-            // 20237
-            int amountOfShapes = scanner.nextInt(); // get total amount of shapes
+			file.getAbsoluteFile(); 
+			
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(file); // scanner takes file object
 
-            this.shapeArray = new Shape[amountOfShapes]; // set the shape arrays size
+			// 20237
+			int amountOfShapes = scanner.nextInt(); // get total amount of shapes
 
-            // for array size - loop
-            for (int i = 0; i < shapeArray.length; i++) {
+			this.shapeArray = new Shape[amountOfShapes]; // set the shape arrays size
 
-                String shapeName = scanner.next(); // get shape name
-                double height = scanner.nextDouble(); // get shape height
-                double radiusOrSide = scanner.nextDouble(); // get shape side or radius
+			// for array size - loop
+			for (int i = 0; i < shapeArray.length; i++) {
 
-                // condition for shape name
-                switch (shapeName) {
+				String shapeName = scanner.next(); // get shape name
+				double height = scanner.nextDouble(); // get shape height
+				double radiusOrSide = scanner.nextDouble(); // get shape side or radius
 
-                    // if shape name - create shape object and add to shape array
-                    // debugger prints out shape name created if true
-                    case "Cone":
-                        Cone cone = new Cone(shapeName, height, radiusOrSide);
-                        shapeArray[i] = cone;
+				// condition for shape name
+				switch (shapeName) {
 
-                        debug.output(1, shapeName + " " + "created");
-                        break;
+				// if shape name - create shape object and add to shape array
+				// debugger prints out shape name created if true
+				case "Cone":
+					Cone cone = new Cone(shapeName, height, radiusOrSide);
+					shapeArray[i] = cone;
 
-                    case "Cylinder":
-                        Cylinder cylinder = new Cylinder(shapeName, height, radiusOrSide);
-                        shapeArray[i] = cylinder;
-                        debug.output(1, shapeName + " " + "created");
-                        break;
+					debug.output(1, shapeName + " " + "created");
+					break;
 
-                    case "Pyramid":
-                        Pyramid pyramid = new Pyramid(shapeName, height, radiusOrSide);
-                        shapeArray[i] = pyramid;
-                        debug.output(1, shapeName + " " + "created");
-                        break;
+				case "Cylinder":
+					Cylinder cylinder = new Cylinder(shapeName, height, radiusOrSide);
+					shapeArray[i] = cylinder;
+					debug.output(1, shapeName + " " + "created");
+					break;
 
-                    case "OctagonalPrism":
-                        OctagonalPrism octegonalPrism = new OctagonalPrism(shapeName, height, radiusOrSide);
-                        shapeArray[i] = octegonalPrism;
-                        debug.output(1, shapeName + " " + "created");
-                        break;
+				case "Pyramid":
+					Pyramid pyramid = new Pyramid(shapeName, height, radiusOrSide);
+					shapeArray[i] = pyramid;
+					debug.output(1, shapeName + " " + "created");
+					break;
 
-                    case "PentagonalPrism":
-                        PentagonalPrism pentagonalPrism = new PentagonalPrism(shapeName, height, radiusOrSide);
-                        shapeArray[i] = pentagonalPrism;
-                        debug.output(1, shapeName + " " + "created");
-                        break;
+				case "OctagonalPrism":
+					OctagonalPrism octegonalPrism = new OctagonalPrism(shapeName, height, radiusOrSide);
+					shapeArray[i] = octegonalPrism;
+					debug.output(1, shapeName + " " + "created");
+					break;
 
-                    case "SquarePrism":
-                        SquarePrism squarePrism = new SquarePrism(shapeName, height, radiusOrSide);
-                        shapeArray[i] = squarePrism;
-                        debug.output(1, shapeName + " " + "created");
-                        break;
+				case "PentagonalPrism":
+					PentagonalPrism pentagonalPrism = new PentagonalPrism(shapeName, height, radiusOrSide);
+					shapeArray[i] = pentagonalPrism;
+					debug.output(1, shapeName + " " + "created");
+					break;
 
-                    case "TriangularPrism":
-                        TriangularPrism triangularPrism = new TriangularPrism(shapeName, height, radiusOrSide);
-                        shapeArray[i] = triangularPrism;
-                        debug.output(1, shapeName + " " + "created");
-                        break;
+				case "SquarePrism":
+					SquarePrism squarePrism = new SquarePrism(shapeName, height, radiusOrSide);
+					shapeArray[i] = squarePrism;
+					debug.output(1, shapeName + " " + "created");
+					break;
 
-                    default:
-                        System.out.println("No shape Found"); // no shape found
-                    // break;
+				case "TriangularPrism":
+					TriangularPrism triangularPrism = new TriangularPrism(shapeName, height, radiusOrSide);
+					shapeArray[i] = triangularPrism;
+					debug.output(1, shapeName + " " + "created");
+					break;
 
-                }
+				default:
+					System.out.println("No shape Found"); // no shape found
+					// break;
 
-            }
+				}
 
-        } catch (FileNotFoundException e) { // catch FileNotFoundException
-            e.printStackTrace();
-        }
+			}
 
-        return shapeArray; // return shape array
+		} catch (FileNotFoundException e) { // catch FileNotFoundException
+			e.printStackTrace();
+		}
 
-    }
+		return shapeArray; // return shape array
 
-    /* GETTER AND SETTERS */
-    public String getPath() {
-        return path;
-    }
+	}
 
-    public void setPath(String path) {
-        this.path = path;
-    }
+	/* GETTER AND SETTERS */
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
 
 }
